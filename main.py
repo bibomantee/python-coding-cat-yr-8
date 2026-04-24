e3a3c95414f845c9b5780326c1305bfde91d27e2
JEREMY LAW <jeremy.law@student.education.wa.edu.au>
2026-04-24 23:46:04 +0800

    finished the triangle thing and now my code is able to work out what all the numbers previously added will create. i said this really badly but what i mean is like if i put 6. it will be 1+2+3+4 +5+6 and it will give me the sum

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-input("Hi")
+
+def printdots(n):
+    if n > 0:
+        print("." * n)
+        printdots(n-1)
+    
+printdots(5)
+
+def startriangle(n):
+    for i in range(n, 0, -1):
+        print('*' * i)
+        
+startriangle(5)
+
+def triangular (n):
+    return sum (range(1, n+1))
+        
+    
+print(triangular(6))
