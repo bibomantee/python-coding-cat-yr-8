28bd6cba8e75c2b0c41b452f27340bb296274ea0
JEREMY LAW <jeremy.law@student.education.wa.edu.au>
2026-04-30 05:49:45 +0800

    im on question 8 but idk why the values are not working or being put in

--- Question8.py
+++ Question8.py
@@ -1,28 +1,28 @@
 import turtle
 
-n = int(input("tree level "))
-length = int(input("length "))
-angle  = int(input("angle "))
-scale  = int(float(input("scale ")))
 
-from numpy import angle
+n = int(input("tree level "))
+length = int(input("length pixels "))
+angle  = int(input("angle between branches "))
+scale  = (float(input("scale or size ")))
 
 def fractaltree(n, length, angle, scale):
      if n == 0:
         return
-    
-turtle.forward(length)
-turtle.left(angle)
-fractaltree(n - 1, length * scale, angle, scale)
-turtle.right(2 * angle)
-fractaltree(n - 1, length * scale, angle, scale)
-turtle.left(angle)
-turtle.backward(length)
+     turtle.forward(length)
+     turtle.left(angle)
+     fractaltree(n - 1, length * scale, angle, scale)
+     turtle.right(2 * angle)
+     fractaltree(n - 1, length * scale, angle, scale)
+     turtle.left(angle)
+     turtle.backward(length)
 
 turtle.speed(0)
-turtle.left(90)
 turtle.penup()
 turtle.goto(0, -200)
-fractaltree
+turtle.setheading(90)
+turtle.pendown()
 
-turtle.done()
+fractaltree(n, length, angle, scale)
+
+turtle.exitonclick()
